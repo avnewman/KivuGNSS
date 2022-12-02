27173f6be165d013b9482fd88cebf439640d038f
Andrew Newman <anewman@gatech.edu>
2022-12-02 13:48:01 -0500

    Auto-update: Fri Dec  2 13:48:01 EST 2022

---
+++ bin/run_all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+pushd /home/anewman/GPS/Kivu/KivuGNSS
+   # get into environment
+   #conda activate Maps
+   # pull data from UNAVCO
+   ./bin/get_UNAVCO_Slns.py
+   # get rates and plot timeseries results
+   ./bin/get_rates.py
+   #plot vector map
+   conda run -n Maps ./bin/Kivu_GNSS_Map.py
+   
+   ./bin/gitpush.sh
+popd
