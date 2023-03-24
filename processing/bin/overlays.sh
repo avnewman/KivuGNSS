2b7bcce6951bdf977840264ec1ede72f0504f681
Andrew Newman <anewman@gatech.edu>
2023-03-24 03:33:48 -0400

    Auto-update: Fri Mar 24 03:33:47 EDT 2023

--- processing/bin/overlays.sh
+++ processing/bin/overlays.sh
@@ -4,8 +4,7 @@
 thumbdir=./processing/Logos
 plotdir=./plots
 
-BASE=$plotdir/KIVU_GNSS.png
-OUT=$plotdir/`basename $BASE .png`_ov.png
+files="$plotdir/KIVU_GNSS.png  $plotdir/KIVU_GNSS_common.png"
 
 # create thumbs on andy's laptop 
 KIVU=/Users/anewman/Documents/Grants/Kivu2022-2024/Logo/Kivu-Logo.png 
@@ -18,21 +17,25 @@ NSFth=$thumbdir/NSF_th.png
 GTth=$thumbdir/GT_th.png
 TULth=$thumbdir/TUL_th.png
 
-# only do 1
-    inaction="-verbose -units "PixelsPerInch" -density 100 "
-    outaction=" -resize 200x200 "
-    outaction=" -resize 180x180 "
+# only do once
+ #   inaction="-verbose -units "PixelsPerInch" -density 100 "
+ #   outaction=" -resize 200x200 "
+ #   outaction=" -resize 180x180 "
  #   convert $inaction $KIVU $outaction  $KIVUth
  #   convert $inaction $NSF  $outaction  $NSFth
  #   convert $inaction $GT   $outaction  $GTth
  #   convert $inaction $TUL   $outaction  $TULth
 
-convert $BASE \
+
+for file in $files
+do 
+  OUT=$plotdir/`basename $file .png`_ov.png
+  convert $file \
    $KIVUth -geometry +160+70 -composite \
    $NSFth -geometry +160+1960 -composite \
    $GTth -geometry +360+1960 -composite \
    $TULth -geometry +560+1980 -composite \
    $OUT
-
-rm $BASE
-mv $OUT $BASE
+  rm $file
+  mv $OUT $file
+done
